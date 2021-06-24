b9157da4354067494f55772a1b429a120e61b616
LuisRetana <luisretana123@gmail.com>
2021-06-24 13:06:00 -0400

    added else if to promptfight variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 
 // You can also log multiple values at once like this
@@ -15,7 +16,8 @@ var enemyAttack = 12;
 var fight = function() {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
-
+var promptFight = window.prompt(" Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+if (promptFight === "fight" || promptFight === "FIGHT"){
   // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
@@ -40,5 +42,21 @@ var fight = function() {
       window.alert(playerName + " has died!");
     } else {
       window.alert(playerName + " still has " + playerHealth + " health left.");
-    }};
-  fight()
+    }}
+    
+    else if (promptFight === "skip" || promptFight === "SKIP"){
+        
+    var confirmSkip = window.confirm("Are you sure you want to skip?");
+    if (confirmSkip) {
+        window.alert(playerName + " has decided to  skip this fight. Goodbye!");
+        playerMoney = playerMoney-2;
+    }
+    else{ 
+      fight();}}
+      else {
+          window.alert("You need to pick a valid option. Try again!");
+      }
+    };
+
+    fight();
+
